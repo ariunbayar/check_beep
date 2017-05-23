3dfd22a9d3b46efac6140c10993dee5476a52bb7
Ariunbayar Baterdene <1480970+ariunbayar@users.noreply.github.com>
2017-05-23 10:52:06 +0800

    Pings or loads web page for availability

--- py-app-indicator.py
+++ py-app-indicator.py
@@ -1,17 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import os
 import signal
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('AppIndicator3', '0.1')
 from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
 from gi.repository import GObject
 from threading import Thread
 import time
 from random import randint
+import subprocess
+import ssl
+import urllib2
 
 
 APPINDICATOR_ID = 'myappindicator'
+indicator = None
+iplist = [
+    ('icmp', '122.201.23.145', 'Unitel',        '#f44336'),
+    ('icmp', '10.10.10.1',     'cisco 2800',    '#f44336'),
+    ('icmp', '10.10.10.50',    'IPPBX',         '#2196f3'),
+    ('icmp', '122.201.23.151', 'Backup server', '#4caf50'),
+    # ('icmp', '122.201.23.146', 'App server',    '#4caf50'),
+    ('http', 'https://122.201.23.146:8000', 'SMSPro',       '#e91e63'),
+    ('http', 'https://122.201.23.146:5000', 'TeamProgress', '#e91e63'),
+    ('http', 'https://evisa.mn',            'eVisa',        '#e91e63'),
+    ('http', 'https://immigration.gov.mn',  b'ГИХГ сайт',    '#e91e63'),
+]
+
 
 def build_menu():
     menu = gtk.Menu()
+    for protocol, ip, desc, color in iplist:
+        menuitem = gtk.MenuItem(protocol + ' - ' + ip + ' - ' + desc)
+        menu.append(menuitem)
     item_quit = gtk.MenuItem('Quit')
     item_quit.connect('activate', quit)
     menu.append(item_quit)
@@ -23,33 +47,55 @@ def quit(source):
     gtk.main_quit()
 
 
+def get_svg_bar(offsetx, height, width, border, h, color):
+    svg_bar = ""
+    svg_bar += """<rect x="%s" y="0" width="%s" height="%s" style="fill:#ccc"/>""" % (offsetx, width, height)
+    x = offsetx + border
+    y = height - h - border
+    w = width - 2 * border
+    svg_bar += """<rect x="%s" y="%s" width="%s" height="%s" style="fill:%s"/>""" % (x, y, w, h, color)
+    return svg_bar
+
+
+def ping_alive(ip):
+    ping_response = subprocess.Popen(
+        ["/bin/ping", "-c1", "-w10", ip], stdout=subprocess.PIPE
+    ).stdout.read()
+    return 'Destination Host Unreachable' not in ping_response
+
+
+def http_alive(url):
+    # TODO quite immature -> ssl._create_unverified_context()
+    rsp = urllib2.urlopen(url, context=ssl._create_unverified_context())
+    return rsp.getcode() == 200
+
+
 def action():
     global indicator
 
-    num_loop = 100
-    while num_loop > 0:
-        time.sleep(0.1)
-        num_loop -= 1
-        #indicator.set_icon(gtk.STOCK_INFO)
-        file = 'icon%s.svg' % (num_loop % 2)
-        with open(file, 'w') as f:
-            svg =  """<?xml version="1.0" encoding="UTF-8" standalone="no"?>"""
-            svg += """<svg width="100" height="4">"""
-            svg += """<rect width="%s" height="4" style="fill:rgb(0,0,255);stroke-width:1;stroke:rgb(255,255,0)">""" % num_loop
-            svg += """<animate attributeType="XML" attributeName="x" from="0" to="50" dur="2s" repeatCount="indefinite"/>"""
-            svg += """</rect>"""
-            svg += """</svg>"""
-            f.write(svg)
-
-        if num_loop < 20:
-            num_loop += randint(0, 40)
+    n = 0
+    while True:
+        filename = 'icon%s.svg' % (n % 2)
+        with open(filename, 'w') as f:
+            f.write("""<?xml version="1.0" encoding="UTF-8" standalone="no"?>""")
+            f.write("""<svg width="%s" height="16">""" % (len(iplist) * 9 - 1))
+            offset = 0
+            for protocol, ip_or_url, desc, color in iplist:
+                if protocol == 'http':
+                    amount = http_alive(ip_or_url) and 14 or 0
+                elif protocol == 'icmp':
+                    amount = ping_alive(ip_or_url) and 14 or 0
+                else:
+                    amount = 0
+                f.write(get_svg_bar(offset, 16, 8, 1, amount, color))
+                offset += 9
+            f.write("""</svg>""")
 
         GObject.idle_add(
             indicator.set_icon,
-            os.path.abspath(file),
+            os.path.abspath(filename),
             priority=GObject.PRIORITY_DEFAULT
         )
-        break
 
         #GObject.idle_add(
         #    indicator.set_label,
@@ -57,14 +103,17 @@ def action():
         #    priority=GObject.PRIORITY_DEFAULT
         #)
 
-
-
-indicator = None
+        n += 1 
+        time.sleep(3)
 
 
 def main():
     global indicator
-    indicator = appindicator.Indicator.new(APPINDICATOR_ID, gtk.STOCK_INFO, appindicator.IndicatorCategory.SYSTEM_SERVICES)
+    indicator = appindicator.Indicator.new(
+        APPINDICATOR_ID,
+        gtk.STOCK_INFO,
+        appindicator.IndicatorCategory.SYSTEM_SERVICES
+    )
     indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
     indicator.set_menu(build_menu())
 
